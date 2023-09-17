5dacf1959acf22f3d01df862f3fc9cacfa6fd41b
aletkw00 <alessiobonizzato@gmail.com>
2023-09-17 20:49:17 +0200

    doc

--- flaskr/static/index.js
+++ flaskr/static/index.js
@@ -12,20 +12,18 @@ function selectModelDir(dir) {
     document.getElementById('model_dir_list').style.display = 'none';
 }
 
-// Show the list of uploaded files
-const fileInput = document.getElementById('fileInput');
-const fileList = document.getElementById('file-list');
+function updateFileName(inputId) {
+    const input = document.getElementById(inputId);
+    const fileName = input.files[0] ? input.files[0].name : ''; // Ottieni il nome del primo file o una stringa vuota
+    const label = input.nextElementSibling; // Ottieni l'etichetta successiva all'input
 
-fileInput.addEventListener('change', (event) => {
-    const files = event.target.files;
-    displayFileNames(files);
-});
+    label.innerHTML = fileName;
+}
 
-function displayFileNames(files) {
-    fileList.innerHTML = '';
-    for (const file of files) {
-        const listItem = document.createElement('div');
-        listItem.textContent = file.name;
-        fileList.appendChild(listItem);
-    }
+function updateFileNames(inputId) {
+    const input = document.getElementById(inputId);
+    const fileNames = Array.from(input.files).map(file => file.name).join(', ');
+    const label = input.nextElementSibling; // Ottieni l'etichetta successiva all'input
+
+    label.innerHTML = fileNames || 'Choose files';
 }
